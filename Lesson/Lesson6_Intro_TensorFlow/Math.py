6b722609c0b8533cc2f4e66b546cbffe329d61b4
Liang XU <liangxuav@gmail.com>
2017-08-10 15:22:43 -0400

    add tensorFlow Lab

--- Lesson/Lesson6_Intro_TensorFlow/Math.py
+++ Lesson/Lesson6_Intro_TensorFlow/Math.py
@@ -5,6 +5,16 @@ x = tf.constant(10)
 y = tf.constant(2)
 z = tf.subtract(tf.divide(x,y),tf.cast(tf.constant(1),tf.float64))
 
+init = tf.global_variables_initializer()
+
+n_features = 120
+n_labels = 5
+weights = tf.Variable(tf.truncated_normal((n_features, n_labels)))
+reduce_SUM = tf.log(tf.cast(([1, 2, 3, 4, 5]),tf.float64))
+
+
 with tf.Session() as sess:
     output = sess.run(z)
     print(output)
+    sess.run(init)
+    print(sess.run(reduce_SUM))

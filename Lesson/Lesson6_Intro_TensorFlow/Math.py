5b10073a8c681d0aecbd6f15c41fdf0363f3c3fe
Liang XU <liangxuav@gmail.com>
2017-08-09 20:46:12 -0400

    add Project 2 folder

---
+++ Lesson/Lesson6_Intro_TensorFlow/Math.py
@@ -0,0 +1,10 @@
+# Solution is available in the other "solution.py" tab
+import tensorflow as tf
+
+x = tf.constant(10)
+y = tf.constant(2)
+z = tf.subtract(tf.divide(x,y),tf.cast(tf.constant(1),tf.float64))
+
+with tf.Session() as sess:
+    output = sess.run(z)
+    print(output)
--- Lesson/Lesson6_Intro_TensorFlow/intro.py
+++ Lesson/Lesson6_Intro_TensorFlow/intro.py
@@ -11,8 +11,16 @@ import tensorflow as tf
 
 # Create TensorFlow object called tensor
 hello_constant = tf.constant('Hello World!')
+A = tf.constant(1234)
+B = tf.constant([12, 34,5,6,78])
+C = tf.constant([ [123,456,789], [222,333,444] ])
+x = tf.placeholder(tf.string)
 
 with tf.Session() as sess:
     # Run the tf.constant operation in the session
     output = sess.run(hello_constant)
     print(output)
+    output2 = sess.run(A)
+    print(output2)
+    output3 = sess.run(x,feed_dict={x:"Hello world Tensor Flow"})
+    print(output3)
